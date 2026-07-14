c4ad584d201cf1b47ceeef7c2b79804ec432866f
PianoTunes <carlos.vega2001@outlook.es>
2026-07-14 12:48:50 +0200

    Commit inicial

---
+++ index.js
@@ -0,0 +1,106 @@
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const app = express();
+
+app.use(express.json());
+
+morgan.token("body", (request) => JSON.stringify(request.body));
+
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body"),
+);
+app.use(cors());
+
+let persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+const generateRandomId = () => {
+  return Math.floor(50000 * Math.random());
+};
+
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  const person = persons.find((person) => person.id == id);
+
+  if (!person) {
+    response.statusMessage = "No hay persona con tal id";
+    return response.status(404).end();
+  }
+
+  response.json(person);
+});
+
+app.get("/info", (request, response) => {
+  const numPeople = persons.length;
+  const now = new Date();
+  response.send(`<div>Phonebook has info for ${numPeople} people</div>
+    <div>${now}</div>`);
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  const person = persons.find((person) => person.id == id);
+  if (!person) {
+    response.statusMessage = "There's no person with such id";
+    response.status(404).end();
+  }
+
+  persons = persons.filter((person) => person.id != id);
+  response.status(204).end();
+});
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+  console.log(request);
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: "Name or number missing",
+    });
+  }
+
+  if (persons.filter((person) => person.name === body.name).length > 0) {
+    return response.status(400).json({
+      error: "Name already existing in phonebook",
+    });
+  }
+
+  const newPerson = {
+    id: generateRandomId(),
+    name: body.name,
+    number: body.number,
+  };
+  persons = persons.concat(newPerson);
+  response.json(newPerson);
+});
+
+const PORT = 3001;
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
